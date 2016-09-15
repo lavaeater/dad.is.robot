b1a7ceb280d3ca2c44607424f2f9ee7d475d2acd
Tommie Nygren <tommie.nygren@gmail.com>
2016-09-15 21:08:13 +0200

    Påbörjade cucumber...

--- www/features/player.feature
+++ www/features/player.feature
@@ -5,6 +5,6 @@ Feature: Moving around
 
     Scenario: Accelerating the player
          Given that the player is standing still
-            And acceleration is .5
+            And acceleration is 0.5
          When I hit accelerate
-         Then the players speed should increase by acceleration
+         Then the players speed should increase by 0.5
---
+++ www/features/steps.js
@@ -0,0 +1,19 @@
+this.Given(/^that the player is standing still$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+});
+
+this.Given(/^acceleration is (\d+)\.(\d+)$/, function (arg1, arg2, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+});
+
+this.When(/^I hit accelerate$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+});
+
+this.Then(/^the players speed should increase by (\d+)\.(\d+)$/, function (arg1, arg2, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+});
