b1783fcde197912346411397957c0150188958af
Tommie Nygren <tommie.nygren@gmail.com>
2016-09-18 11:23:06 +0200

    Lagt till litet program som eventuellt kan köras och som skapar en värld, eventuellt

--- www/js/dad.js
+++ www/js/dad.js
@@ -1,2 +1,21 @@
-var WorldGenerator = require('./world/WorldGenerator');
+var World = require('./world/worldgenerator.js');
+
+/*
+Create and traverse a world!
+*
+*
+*/
+
+var size = 10;
+var world = new World(10, 100);
+
+for (y = 0; y < size; y++) {
+    for (x = 0; x < size; x++) {
+        var tile = world.getTileAt(x,y);
+        console.log("Current tile" + tile.key);
+        console.log("Type: " + tile.type);
+        tile.generateSubTiles();
+        tile.renderSubTiles();
+    }
+}
 
--- www/js/world/worldgenerator.js
+++ www/js/world/worldgenerator.js
@@ -1,109 +1,131 @@
 var _ = require('lodash');
 
-var tileTypes = ['grass', 'water', 'desert', 'mountain', 'forest'];
-var tileRelations = [
-    { grass: [] },
-    { water: [] },
-    { desert: [] },
-    { mountain: [] },
-    { forest: [] }
+var tileTypes = ['g', 'w', 'd', 'm', 'f'];
+var tileGenerators = [
+    { g: grassGenerator },
+    { w: waterGenerator },
+    { d: desertGenerator },
+    { m: mountainGenerator },
+    { f: forestGenerator }
 ];
 
-function KeyToString(x,y) {
+function KeyToString(x, y) {
     return x.toString() + ':' + y.toString();
 };
 
-function World(size, tileSize) {
-    var width = size;
-    var height = size;
-    var tileSize = tileSize;
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
-    var tiles = {};
-    var getTileAt = function (x, y) {
-        var key = KeyToString(x, y);
-        if (_.has(tiles, key)) {
-            return tiles[key];
-        }
-    };
+function basicTileGenerator(x, y) {
+    var randomSeed = randomIntFromInterval(1, 10);
+    var type = '';
+    if (randomSeed <= 4) {
+        type = 'g';
+    } else if (4 < randomSeed && randomSeed <= 7) {
+        type = 'f';
+    } else if (7 < randomSeed && randomSeed <= 8) {
+        type = 'w';
+    } else if (8 < randomSeed && randomSeed <= 9) {
+        type = 'm';
+    } else if (9 < randomSeed && randomSeed <= 10) {
+        type = 'd';
+    }
+    return new Tile(x, y, type, tileGenerators[type]);
+};
 
+var grassGenerator = function (x, y) {
+    var type = '';
+    var randomSeed = randomIntFromInterval(1, 10);
+
+    if (randomSeed <= 5) {
+        type = 'g';
+    } else if (5 < randomSeed && randomSeed <= 7) {
+        type = 'f';
+    } else if (7 < randomSeed && randomSeed <= 9) {
+        type = 'w';
+    } else if (9 < randomSeed && randomSeed <= 10) {
+        type = 'm';
+    }
+    return new SubTile(x, y, type);
+};
+
+var forestGenerator = function (x, y) {
+    var type = '';
+    var randomSeed = randomIntFromInterval(1, 10);
+
+    if (randomSeed <= 5) {
+        type = 'f';
+    } else if (5 < randomSeed && randomSeed <= 7) {
+        type = 'm';
+    } else if (7 < randomSeed && randomSeed <= 9) {
+        type = 'w';
+    } else if (9 < randomSeed && randomSeed <= 10) {
+        type = 'g';
+    }
+    return new SubTile(x, y, type);
+};
+
+var mountainGenerator = function (x, y) {
+    var type = '';
+    var randomSeed = randomIntFromInterval(1, 10);
+
+    if (randomSeed <= 6) {
+        type = 'm';
+    } else if (6 < randomSeed && randomSeed <= 8) {
+        type = 'f';
+    } else if (8 < randomSeed && randomSeed <= 9) {
+        type = 'w';
+    } else if (9 < randomSeed && randomSeed <= 10) {
+        type = 'd';
+    }
+    return new SubTile(x, y, type);
+};
+
+var waterGenerator = function (x, y) {
+    var type = '';
+    var randomSeed = randomIntFromInterval(1, 10);
+
+    if (randomSeed <= 8) {
+        type = 'w';
+    } else if (8 < randomSeed && randomSeed <= 9) {
+        type = 'd';
+    } else if (9 < randomSeed && randomSeed <= 10) {
+        type = 'm';
+    return new SubTile(x, y, type);
+};
+
+var desertGenerator = function (x, y) {
+    var type = '';
+    var randomSeed = randomIntFromInterval(1, 10);
+
+    if (randomSeed <= 7) {
+        type = 'd';
+    } else if (7 < randomSeed && randomSeed <= 9) {
+        type = 'm';
+    } else if (9 < randomSeed && randomSeed <= 10) {
+        type = 'w';
+    }
+    return new SubTile(x, y, type);
+};
+
+function SubTile(x, y, type) {
+    var self = this;
+    var key = KeyToString(x, y);
+    var type = type;
+    var x = x;
+    var y = y;
     return {
-        width: width,
-        height: height,
-        getTileAt: getTileAt
+        key: key,
+        x: x,
+        y: y,
+        type: type
     };
-};
-
-function randomIntFromInterval(min,max)
-{
-    return Math.floor(Math.random()*(max-min+1)+min);
-}
-
-
-var grassGenerator = function(x,y) {
-    var type = '';
-    var randomSeed = randomIntFromInterval(1, 10);
-
-    if(randomSeed <= 5) {
-        type = 'g';
-    } else if(5 < randomSeed && randomSeed <=7) {
-        type = 'f';
-    } else if(7 < randomSeed && randomSeed <=9) {
-        type = 'w';
-    } else if(9 < randomSeed && randomSeed <= 10) {
-        type = 'm';
-    }
-    return new SubTile(x, y, type);
-};
-
-var forestGenerator = function(x,y) {
-    var type = '';
-    var randomSeed = randomIntFromInterval(1, 10);
-
-    if(randomSeed <= 5) {
-        type = 'f';
-    } else if(5 < randomSeed && randomSeed <=7) {
-        type = 'm';
-    } else if(7 < randomSeed && randomSeed <=9) {
-        type = 'w';
-    } else if(9 < randomSeed && randomSeed <= 10) {
-        type = 'g';
-    }
-    return new SubTile(x, y, type);
-};
-
-var mountainGenerator = function(x,y) {
-    var type = '';
-    var randomSeed = randomIntFromInterval(1, 10);
-
-    if(randomSeed <= 6) {
-        type = 'm';
-    } else if(6 < randomSeed && randomSeed <=8) {
-        type = 'f';
-    } else if(8 < randomSeed && randomSeed <=9) {
-        type = 'w';
-    } else if(9 < randomSeed && randomSeed <= 10) {
-        type = 'd';
-    }
-    return new SubTile(x, y, type);
-};
-
-var desertGenerator = function(x,y) {
-    var type = '';
-    var randomSeed = randomIntFromInterval(1, 10);
-
-    if(randomSeed <= 7) {
-        type = 'd';
-    } else if(7 < randomSeed && randomSeed <=9) {
-        type = 'm';
-    } else if(9 < randomSeed && randomSeed <= 10) {
-        type = 'w';
-    }
-    return new SubTile(x, y, type);
 };
 
 function Tile(x, y, type, subTileGenerator) {
     var self = this;
-    var key = KeyToString(x,y);
+    var key = KeyToString(x, y);
     var x = x;
     var y = y;
     var type = type;
@@ -114,7 +136,7 @@ function Tile(x, y, type, subTileGenerator) {
     var generateSubTiles = function () {
         for (i = 0; i < size; i++) {
             for (j = 0; j < size; j++) {
-                var tile = self.subTileGenerator(j,i);
+                var tile = self.subTileGenerator(j, i);
                 subTiles[tile.key] = tile;
             }
         }
@@ -124,7 +146,7 @@ function Tile(x, y, type, subTileGenerator) {
         for (i = 0; i < size; i++) {
             var row = '';
             for (j = 0; j < size; j++) {
-                var key = KeyToString(j,i);
+                var key = KeyToString(j, i);
                 row += subTiles[key].type;
             }
             console.log(row);
@@ -137,20 +159,34 @@ function Tile(x, y, type, subTileGenerator) {
         y: y,
         type: type,
         subTiles: subTiles,
-        renderSubTiles: renderSubTiles
+        renderSubTiles: renderSubTiles,
+        generateSubTiles: generateSubTiles
     };
 };
 
-function SubTile(x, y, type) {
-    var self = this;
-    var key = KeyToString(x,y);
-    var type = type;
-    var x = x;
-    var y = y;
+function World(size, tileSize) {
+    var width = size;
+    var height = size;
+    var tileSize = tileSize;
+
+    var tiles = {};
+    var getTileAt = function (x, y) {
+        var key = KeyToString(x, y);
+        if (_.has(tiles, key)) {
+            //tile exists, return it
+            return tiles[key];
+        } else {
+            //tile does not exist, create it!
+            tiles[key] = basicTileGenerator(x, y);
+            return tiles[key];
+        }
+    }
+
     return {
-        key: key,
-        x: x,
-        y: y,
-        type: type
+        width: width,
+        height: height,
+        getTileAt: getTileAt
     };
 };
+
+module.exports = World;
