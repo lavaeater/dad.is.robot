dcb788b63870d5b280eb095cb119a3fa8b689117
Tommie Nygren <tommie.nygren@gmail.com>
2016-09-20 11:24:55 +0200

    La in ref till questengine i world.html

--- www/js/quest/questengine.js
+++ www/js/quest/questengine.js
@@ -1,11 +1,14 @@
 function StoryStep(title) {
     var self = this;
-
+    return {};
 };
 
 function QuestEngine() {
     var self = this;
-
-    
+    return {};
 };
 
+function EncounterEngine() {
+    var self = this;
+    return {};
+};
