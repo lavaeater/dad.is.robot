96b858ccf7febfc6a2ab7b31140da4e6b110e572
Tommie Nygren <tommie.nygren@gmail.com>
2016-09-21 11:07:20 +0200

    Coming around!

--- www/js/quest/questengine.js
+++ www/js/quest/questengine.js
@@ -1,36 +1,53 @@
+function Random(max, min) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
 function StoryStep(title) {
     var self = this;
     return {
-
+        title: title
     };
 };
 
 function QuestEngine() {
     var self = this;
-    return {
 
+    var generate = function () {
+        var numberOfSteps = Random(5,1);
+        var steps = [];
+        for(i = 0; i < numberOfSteps; i++) {
+            steps.push(new StoryStep())
+        }
+    };
+
+    return {
+        generate: generate
     };
 };
 
-function Encounter(key, type, title) {
+function Encounter(key, type, title, storySteps) {
     return {
         key: key,
         type: type,
-        title: title
+        title: title,
+        storySteps: storySteps
     }
 };
 
 function EncounterGenerator() {
     var self = this;
-    self.random = function (max, min) {
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    };
+
+    var questEngine = new QuestEngine();
+
     var generateEncounter = function (key) {
-        var seed = self.random(100,1);
-        if(seed <= 85) {
+        var seed = self.random(100, 1);
+        if (seed <= 10) {
+            return new Encounter(key, 'quest', 'En förbipasserande ser din robot och berättar att hen har hört rykten om ett skrotupplag med delar');
+        }
+        if (seed <= 85) {
             return new Encounter(key, 'empty', 'Nothing happening here');
         }
-        if(seed <= 100) {
+        if (seed <= 100) {
             return new Encounter(key, 'ruin', 'There is a ruin down there. Investigate?');
         }
     };
