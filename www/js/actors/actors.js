adf4d6f23a8fb0a7a1a68eed29e436aa47cff58d
Tommie Nygren <tommie.nygren@gmail.com>
2016-09-23 10:28:21 +0200

    Actioncards

--- www/js/actors/actors.js
+++ www/js/actors/actors.js
@@ -1,3 +1,10 @@
 function Actor(name, attributes, attacks, defenses) {
-    return _.assign({name:name}, attributes, attacks, defenses);
+    return _.assign({ name: name }, attributes, attacks, defenses);
+}
+
+function Action(name, type) {
+    return {
+        name: name,
+        type: type
+    };
 }
