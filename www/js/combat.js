aff782ccc76a57c6c350aed24b3f61f4f7e9939c
Tommie Nygren <tommie.nygren@gmail.com>
2016-09-29 11:18:04 +0200

    Påbörjat state machine med att definiera några states

--- www/js/combat.js
+++ www/js/combat.js
@@ -1,3 +1,21 @@
+/*
+
+Nu ska vi bygga en statemachine för strid
+
+Vilka states har vi? Listan nedan kan förändras men man kan tänka sig ett start-state, t.ex.
+
+start
+pickmoves
+combat
+done
+
+start kommer man till via någon funktion som tar emot en lista av protagonister och antagonister.
+Från start kommer man till pickmoves i princip automatiskt
+pickmoves börjar med protagonisterna och låter dem välja moves.
+
+*/
+
+
 function CombatRound() {
 
     return {
