b93ce17e78e45153500ec8136e8788db85b3271f
Tommie Nygren <tommie.nygren@gmail.com>
2016-10-07 13:08:33 +0200

    Projekt

---
+++ game/dad.is.robot.combat/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dad.@is.robot.combat
+{
+    public class Class1
+    {
+    }
+}
---
+++ game/dad.is.robot.game/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Otter;
+
+namespace dad.@is.robot.game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var game = new Game("dad is robot", 1600,900,60, true);
+            //game.Start();
+        }
+    }
+}
