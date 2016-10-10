b6416a069533703e5cd1827706b25b3b35f54cb3
Tommie Nygren <tommie.nygren@gmail.com>
2016-10-10 08:47:47 +0200

    Lagt till projekt etc i en otter-branch

---
+++ game/dad.is.robot.combat/CombatEngine.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Appccelerate.StateMachine;
+
+namespace dad.@is.robot.combat
+{
+    public class CombatEngine
+    {
+        public CombatEngine()
+        {
+            CombatStateMachine = CombatStateMachine.Create();
+        }
+
+        public CombatStateMachine CombatStateMachine { get; set; }
+    }
+
+    public class CombatStateMachine
+    {
+        public PassiveStateMachine<States, Events> StateMachine { get; set; }
+        public static CombatStateMachine Create()
+        {
+            var cms = new CombatStateMachine();
+            cms.StateMachine = new PassiveStateMachine<States, Events>();
+            cms.StateMachine
+                .In(States.BeforeCombat)
+                .On(Events.Start)
+                .Goto(States.ProtagonistPicking);
+
+            cms.StateMachine
+                .In(States.ProtagonistPicking)
+                .On(Events.ProtagonistPicked)
+                .Goto(States.AntagonistPicking);
+
+            cms.StateMachine
+                .In(States.AntagonistPicking)
+                .On(Events.AntagonistPicked)
+                .Goto(States.ResolveCombat);
+            cms.StateMachine
+                .In(States.ResolveCombat)
+                .On(Events.CombatRoundResolved)
+                .If()
+            {
+
+            }
+            return cms;
+        }
+
+        private static bool Guard()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public enum States
+    {
+        BeforeCombat,
+        ProtagonistPicking,
+        AntagonistPicking,
+        ResolveCombat,
+        FightOver
+    }
+
+    public enum Events
+    {
+        Start,
+        ProtagonistPicked,
+        AntagonistPicked,
+        CombatRoundResolved,
+        CombatOver
+    }
+}
