8b900572bee7bbd9f14b2c60ad2f98caa93d625f
Tommie Nygren <tommie.nygren@gmail.com>
2016-10-26 23:43:49 +0200

    Yay. Statemachine skapad, that wasn't so hard was it?

--- game/robot.dad.combat/CombatEngine.cs
+++ game/robot.dad.combat/CombatEngine.cs
@@ -1,69 +1,105 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Appccelerate.StateMachine;
 
 namespace robot.dad.combat
 {
     public class CombatEngine
     {
+        public List<Combattant> Participants => new List<Combattant>();
+        public PassiveStateMachine<States, Events> StateMachine { get; set; }
+        public int Round { get; set; }
+
+        public CombatEngine(List<Combattant> participants) :this()
+        {
+            Participants.AddRange(participants);
+        }
         public CombatEngine()
         {
-            CombatStateMachine = CombatStateMachine.Create();
-        }
-
-        public CombatStateMachine CombatStateMachine { get; set; }
-    }
-
-    public class CombatStateMachine
-    {
-        public PassiveStateMachine<States, Events> StateMachine { get; set; }
-        public static CombatStateMachine Create()
-        {
-            var cms = new CombatStateMachine();
-            cms.StateMachine = new PassiveStateMachine<States, Events>();
-            cms.StateMachine
+            StateMachine = new PassiveStateMachine<States, Events>();
+            StateMachine
                 .In(States.BeforeCombat)
                 .On(Events.Start)
-                .Goto(States.ProtagonistPicking);
+                .Goto(States.PlayerPicking);
 
-            cms.StateMachine
-                .In(States.ProtagonistPicking)
-                .On(Events.ProtagonistPicked)
-                .Goto(States.AntagonistPicking);
+            StateMachine
+                .In(States.PlayerPicking)
+                .ExecuteOnEntry(PickMove)
+                .On(Events.PlayerPicked)
+                .If(() => AllPlayersHavePicked).Goto(States.ResolveCombat)
+                .If(() => !AllPlayersHavePicked).Goto(States.PlayerPicking);
 
-            cms.StateMachine
-                .In(States.AntagonistPicking)
-                .On(Events.AntagonistPicked)
-                .Goto(States.ResolveCombat);
-            cms.StateMachine
+            StateMachine
                 .In(States.ResolveCombat)
+                .ExecuteOnEntry(ResolveCombatRound)
                 .On(Events.CombatRoundResolved)
-                .If()
-            {
+                .Goto(States.PlayerPicking);
 
-            }
-            return cms;
+            StateMachine
+                .In(States.ResolveCombat)
+                .On(Events.CombatOver)
+                .Goto(States.CombatOver);
+
+            StateMachine
+                .In(States.CombatOver)
+                .ExecuteOnEntry(CombatOver);
+            StateMachine.Initialize(States.BeforeCombat);
         }
 
-        private static bool Guard()
+        public void StartCombat()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Start the fight");
+            StateMachine.Start();
+            StateMachine.Fire(Events.Start);
         }
+
+        private void CombatOver()
+        {
+            Console.WriteLine("Combat over!!");
+        }
+
+        public void PickMove()
+        {
+            var playerToPickFor = Participants.First(p => !p.HasPicked);
+            playerToPickFor.PickMove();
+            StateMachine.Fire(Events.PlayerPicked);
+        }
+
+        public void ResolveCombatRound()
+        {
+            Round++;
+            Console.WriteLine($"Round {Round}!");
+            //This will be a doozy
+            StateMachine.Fire(Events.CombatRoundResolved);            
+        }
+
+        public bool AllPlayersHavePicked => Participants.TrueForAll(p => p.HasPicked);
+    }
+
+    public class Combattant
+    {
+        public void PickMove()
+        {
+            HasPicked = true;
+        }
+
+        public bool HasPicked { get; set; }
+        public bool Npc { get; set; }
     }
 
     public enum States
     {
         BeforeCombat,
-        ProtagonistPicking,
-        AntagonistPicking,
+        PlayerPicking,
         ResolveCombat,
-        FightOver
+        CombatOver
     }
 
     public enum Events
     {
         Start,
-        ProtagonistPicked,
-        AntagonistPicked,
+        PlayerPicked,
         CombatRoundResolved,
         CombatOver
     }
---
+++ game/robot.dad.tests/CombatEngineTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using robot.dad.combat;
+
+namespace robot.dad.tests
+{
+    [TestFixture]
+    public class CombatEngineTests
+    {
+        [Test]
+        public void TestCombatEngine()
+        {
+            var participants = new List<Combattant>();
+            participants.Add(new Combattant()
+            {
+                Npc = false
+            });
+            participants.Add(new Combattant()
+            {
+                Npc = true
+            });
+
+
+            var ce = new CombatEngine(participants);
+            ce.StartCombat();
+        }
+    }
+}
