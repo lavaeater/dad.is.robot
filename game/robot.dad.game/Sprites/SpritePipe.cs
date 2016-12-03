c6963c3c41dd0204aeda45cf2ec1ca446a27e48f
Tommie Nygren <tommie.nygren@gmail.com>
2016-12-03 08:23:29 +0100

    Fixat statisk klass för sprites

---
+++ game/robot.dad.game/Sprites/SpritePipe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Otter;
+
+namespace robot.dad.game.Sprites
+{
+    public static class SpritePipe
+    {
+        public static Image Ship => new Image("Sprites\\spaceShips_003.png");
+        public static Image Ruin => new Image("Sprites\\ruinsCorner.png");
+        public static Image UnknownTile => new Image("Sprites\\grass_01.png");
+
+    }
+}
