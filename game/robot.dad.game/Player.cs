4945d7a0ceffcdfa119204070ad106e30de0ef31
Tommie Nygren <tommie.nygren@gmail.com>
2016-12-03 16:37:34 +0700

    MAde sure the player sprite is always on top

--- game/robot.dad.game/Player.cs
+++ game/robot.dad.game/Player.cs
@@ -4,13 +4,14 @@
     {
         public Player(float x, float y, string imagePath) : base (x, y, imagePath)
         {
-            
-            
+
         }
 
         public override void Update()
         {
             base.Update();
+            Scene.BringToFront(this);
+
             X = Scene.CameraCenterX;
             Y = Scene.CameraCenterY;
         }
