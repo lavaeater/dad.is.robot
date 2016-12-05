a1dbc741df966b9bc23202be2bff135f09394405
Tommie Nygren <tommie.nygren@gmail.com>
2016-12-05 23:09:54 +0700

    Fixed thrust movement, with graphics. Now aggressors and enemies

--- game/robot.dad.game/Player.cs
+++ game/robot.dad.game/Player.cs
@@ -8,8 +8,7 @@ namespace robot.dad.game
     public class Player : Entity
     {
         public readonly Session Session;
-        private ThrustComponent _thrustComponent;
-
+     
         public Player(float x, float y, Session session)
         {
             //_yOrigin = 200f;//173f;
@@ -17,9 +16,9 @@ namespace robot.dad.game
             Init();
             //hmmm
             var axis = Axis.CreateArrowKeys();
-            var movement = new ThrusterMovement(500, 5, 100, 50, axis);
-            //_thrustComponent = new ThrustComponent(this, _axis);
-            AddComponents(axis, movement);//, _thrustComponent);
+
+            var movement = new ThrusterMovement(3, 5, axis, 90f, 200f, 500f, true);
+            AddComponents(axis, movement);
 
             Session = session;
             X = x;
@@ -32,45 +31,12 @@ namespace robot.dad.game
             AddGraphics(SpritePipe.Ship);
             Graphic.CenterOrigin();
             Graphic.Scale = 0.5f;
-            //SpritePipe.Ship.CenterOrigin();
-            //SpritePipe.Ship.Scale = 0.5f;
-
         }
 
         public override void Update()
         {
             base.Update();
             Scene.BringToFront(this);
-        }
-    }
-
-    public class ThrustComponent : Component
-    {
-        private Player _player;
-        private Axis _axis;
-        private Graphic[] Images = new Graphic[3];
-
-        public ThrustComponent(Player player, Axis _axis)
-        {
-            Images[1] = SpritePipe.ThrustOne;
-            Images[2] = SpritePipe.ThrustTwo;
-            Images[3] = SpritePipe.ThrustThree;
-
-            this._player = player;
-            this._axis = _axis;
-        }
-
-        public override void Update()
-        {
-            ////Set image according to thrust
-            //if (_axis.Y != 0f)
-            //{
-            //    _player.Graphic = _player.Images[3];
-            //}
-            //else
-            //{
-            //    _player.Graphic = _player.Images[0];
-            //}
         }
     }
 }
--- game/robot.dad.game/ThrusterMovement.cs
+++ game/robot.dad.game/ThrusterMovement.cs
@@ -1,75 +1,109 @@
 ﻿using System;
 using Otter;
+using robot.dad.game.Sprites;
 
 namespace robot.dad.game
 {
     public class ThrusterMovement : Movement
     {
-        private readonly float MaxSpeed;
-        private readonly Speed TargetSpeed;
-        private readonly float _rotationSpeed;
+        private float _rotationSpeed;
         private readonly float _drag;
-        private readonly float _roation;
-        private readonly Speed Speed;
         private readonly Axis _axis;
         private float _angle;
-
-        private readonly float _maxAcceleration;
-        private float _currentAcceleration;
-
+        private readonly float _maxThrust;
+        private float _currentThrust;
         private readonly float _maxSpeed;
         private float _currentSpeed;
+        private int _speedX;
+        private int _speedY;
+        private readonly bool _enableThrustGraphic;
+        public bool Freeze;
+        private int _smallThrust;
+        private int _mediumThrust;
 
-        public ThrusterMovement(float maxSpeed, float rotationSpeed, float drag, float acceleration, Axis axis)
+        public ThrusterMovement(float rotationSpeed, float drag, Axis axis, float angle, float maxThrust, float maxSpeed, bool enableThrustGraphic)
         {
-            //Hardcoded values until we get it right!
-            /*
-             * When the player presses the accelerator, acceleration goes up! (of course)
-             * So, acceleration AND speed have maxValues, and currentValues
-             * 
-             * _currentSpeed is then lowered by drag in opposite direction
-             */
-            _angle = 90;
-            _drag = 5;
+            _rotationSpeed = rotationSpeed;
+            _drag = drag;
             _axis = axis;
-            _currentAcceleration = 0;
-            _maxAcceleration = 200;
+            _angle = angle;
+            _maxThrust = maxThrust;
+            _maxSpeed = maxSpeed;
+            _enableThrustGraphic = enableThrustGraphic;
             _currentSpeed = 0;
-            _maxSpeed = 500;
-            _rotationSpeed = 3;
+            _speedX = 0;
+            _speedY = 0;
+            _smallThrust = (int)_maxThrust / 2;
+            _mediumThrust = (int)_maxThrust - (int)_maxThrust / 4;
         }
-
 
         public override void Update()
         {
             if (Freeze) return;
 
-            _angle = Util.ApproachAngle(_angle, Util.WrapAngle(_angle + (_rotationSpeed * -_axis.X)), _rotationSpeed);
-
-            Entity.Graphics.ForEach(g => g.Angle = _angle + 90f);
-
             if (_axis != null)
             {
                 if (Math.Abs(_axis.Y) > 0)
                 {
-                    _currentAcceleration = Util.Approach(_currentAcceleration, _maxAcceleration, 50);
+                    _currentThrust = Util.Approach(_currentThrust, _maxThrust, 5);
                 }
                 else
                 {
-                    _currentAcceleration = 0;
+                    _currentThrust = 0;
                 }
             }
 
-            if (_currentAcceleration > 0)
+            if (_enableThrustGraphic)
             {
-                _currentSpeed = Util.Approach(_currentSpeed, _maxSpeed, _currentAcceleration);
+                int currThrust = (int)_currentThrust;
+                if (0 < currThrust && currThrust < _smallThrust)
+                {
+                    _rotationSpeed = 5;
+                    SmallThrust.Visible = true;
+                }
+                else if (_smallThrust < currThrust && currThrust < _mediumThrust)
+                {
+                    _rotationSpeed = 3;
+                    MediumThrust.Visible = true;
+                }
+                else if (_mediumThrust < currThrust)
+                {
+                    _rotationSpeed = 2;
+                    MaxThrust.Visible = true;
+                }
+                else if (currThrust == 0)
+                {
+                    if (_currentSpeed < 1)
+                    {
+                        _rotationSpeed = 0;
+                    }
+                    else
+                    {
+                        _rotationSpeed = 5;
+                    }
+                    SmallThrust.Visible = false;
+                    MediumThrust.Visible = false;
+                    MaxThrust.Visible = false;
+                }
+            }
+
+            _angle = Util.ApproachAngle(_angle, Util.WrapAngle(_angle + (_rotationSpeed * -_axis.X)), _rotationSpeed);
+
+            Entity.Graphics.ForEach(g => g.Angle = _angle + 90f);
+
+
+
+
+            if (_currentThrust > 0)
+            {
+                _currentSpeed = Util.Approach(_currentSpeed, _maxSpeed, _currentThrust);
             }
             else
             {
                 _currentSpeed = Util.Approach(_currentSpeed, 0, _drag);
             }
             _speedX = (int)Util.PolarX(_angle, _currentSpeed);
-            _speedY = (int) Util.PolarY(_angle, _currentSpeed);
+            _speedY = (int)Util.PolarY(_angle, _currentSpeed);
 
 
             MoveXY(_speedX, _speedY, Collider);
@@ -80,9 +114,31 @@ namespace robot.dad.game
             }
         }
 
-        public bool Freeze;
-        private float Accel;
-        private int _speedX;
-        private int _speedY;
+        private readonly Graphic SmallThrust = SpritePipe.ThrustOne;
+        private readonly Graphic MediumThrust = SpritePipe.ThrustTwo;
+        private readonly Graphic MaxThrust = SpritePipe.ThrustThree;
+
+
+        public override void Added()
+        {
+            if (_enableThrustGraphic)
+            {
+
+                float originY = SmallThrust.ScaledHeight + Entity.Graphic.ScaledHeight / 2;
+                SmallThrust.SetOrigin(SmallThrust.HalfWidth / 2 +1, originY);
+                MediumThrust.SetOrigin(MediumThrust.HalfWidth / 2 +1, originY);
+                MaxThrust.SetOrigin(MaxThrust.HalfWidth / 2 +1, originY);
+
+                SmallThrust.Angle = 90;
+                MediumThrust.Angle = 90;
+                MaxThrust.Angle = 90;
+
+                SmallThrust.Visible = false;
+                MediumThrust.Visible = false;
+                MaxThrust.Visible = false;
+
+                Entity.AddGraphics(SmallThrust, MediumThrust, MaxThrust);
+            }
+        }
     }
 }
