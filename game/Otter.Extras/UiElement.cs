ecaccfb1311c7550c58feeb8b6683e86702093fa
Tommie Nygren <tommie.nygren@gmail.com>
2017-05-16 08:14:04 +0200

    Dialogbörjan

--- game/Otter.Extras/UiElement.cs
+++ game/Otter.Extras/UiElement.cs
@@ -1,4 +1,6 @@
-﻿namespace Otter.Extras
+﻿using System;
+
+namespace Otter.Extras
 {
     public class UiElement : Entity
     {
@@ -6,5 +8,24 @@
         public int Width { get; set; } = 100;
         public int Height { get; set; } = 100;
         public virtual string Tag { get; set; }
+    }
+
+    public class SimpleDialog : UiElement
+    {
+        public Action<DialogResult> DialogAction { get; set; }
+
+        public void Close()
+        {
+            if (Scene != null)
+            {
+                Scene.Remove(this);
+            }
+        }
+    }
+
+    public enum DialogResult
+    {
+        Ok,
+        Cancel
     }
 }
