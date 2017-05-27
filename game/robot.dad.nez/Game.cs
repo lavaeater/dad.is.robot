8e6d03914d0a58edc57b12f31e180a9663649995
Tommie Nygren <tommie.nygren@gmail.com>
2017-05-27 20:59:03 +0200

    Det bygger, men jag kan ännu ej göra något endaste dugg... yay

---
+++ game/robot.dad.nez/Game.cs
@@ -0,0 +1,19 @@
+﻿using Nez;
+
+namespace robot.dad.nez
+{
+    class Game : Core
+    {
+        protected override void Initialize()
+        {
+            base.Initialize();
+            Window.AllowUserResizing = true;
+            scene = new TestScene();
+        }
+    }
+
+    internal class TestScene : Scene
+    {
+
+    }
+}
---
+++ game/robot.dad.nez/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace robot.dad.nez
+{
+    internal static class Program
+    {
+        private static Game game;
+
+        internal static void RunGame()
+        {
+            game = new Game();
+            game.Run();
+            game.Dispose();
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            RunGame();
+        }
+    }
+}
+
