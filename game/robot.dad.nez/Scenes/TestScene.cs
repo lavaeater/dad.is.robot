b14779d2e24983dee5fa97f25ea931c330c21da4
Tommie Nygren <tommie.nygren@gmail.com>
2017-05-28 09:52:06 +0200

    Påbörjat Scene2DUi-grejen, vi ska göra UI. Att rendera en hexkarta i bakgrunden kommer vara en piece of cake

--- game/robot.dad.nez/Scenes/TestScene.cs
+++ game/robot.dad.nez/Scenes/TestScene.cs
@@ -1,20 +1,36 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
 using Nez;
 using Nez.Sprites;
+using Nez.UI;
 
 namespace robot.dad.nez.Scenes
 {
     internal class TestScene : Scene
     {
+        public UICanvas UiCanvas;
+        private Table _table;
         public override void initialize()
         {
             addRenderer(new DefaultRenderer());
+
+            UiCanvas = createEntity("ui")
+                .addComponent(new UICanvas());
+            UiCanvas.isFullScreen = true;
+            UiCanvas.renderLayer = 999;
+            SetupUi();
+
 
             var airshipTexture = content.Load<Texture2D>(Content.Images.airshipfinal);
 
             var ship = createEntity("airship")
                 .addComponent(new Sprite(airshipTexture))
                 .transform.setPosition(Screen.center);
+        }
+
+        private void SetupUi()
+        {
+            _table = UiCanvas.stage.addElement(new Table());
+            _table.setFillParent(true);
         }
     }
 }
