6f6203b81229efedd2b401d37af54bdef9f7d0b2
Tommie Nygren <tommie.nygren@gmail.com>
2017-06-01 10:30:29 +0200

    Lade till QuestContext

--- game/robot.dad.nez/Scenes/TestScene.cs
+++ game/robot.dad.nez/Scenes/TestScene.cs
@@ -48,4 +48,28 @@ namespace robot.dad.nez.Scenes
     {
         
     }
+
+    //The context for the state machine and quest
+    /* Can a quest be abandoned?
+     */
+    interface IQuest
+    {
+        string Title { get; }
+        string Description { get; }
+        string CurrentStepTitle { get; }
+        string CurrentStepDescription { get; }
+        IQuestState CurrentState { get; }
+        //Some mechanism to enumerate possible choices and what data those choices need to contain.
+        //May need changes of state machine implementation
+    }
+
+    interface IQuestState
+    {
+        
+    }
+
+    interface IQuestEvent
+    {
+        
+    }
 }
